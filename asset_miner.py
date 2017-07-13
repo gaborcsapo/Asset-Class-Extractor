8be534ab4a7f6d78bed2a461bab66c8e57a188d0
Gabor <gabor.csapo@nyu.edu>
2017-07-13 16:18:17 +0100

    checking cand len

--- asset_miner.py
+++ asset_miner.py
@@ -97,9 +97,10 @@ def extract(doc_name):
 	
 	candidates = []
 	candidates += text_extract(doc)
-	best = evaluate(candidates)
-	if (best.count() < 7):
-		candidates += table_extract(doc, doc.page_dict) #table_extract(doc, [doc.OFFSET]) #
+	if (len(candidates) > 0):	
+		best = evaluate(candidates)
+		if (best.count() < 7):
+			candidates += table_extract(doc, doc.page_dict) #table_extract(doc, [doc.OFFSET]) #
 	if (len(candidates) == 0):
 		print('#####No candidates found in', doc.title, file=LOG_FILE)
 		LOG_FILE.close()
